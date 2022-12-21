3cde5db508a012ef343981a716b3d60650dbcae0
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-21 16:12:28 +0800

    Create bottom navigation bar in TabScreen

--- lib/Views/tabsScreen.dart
+++ lib/Views/tabsScreen.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("data"),
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.cyan,
+          selectedItemColor: Colors.white,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.list,
+                ),
+                label: "ToDos"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month), label: "Calendar")
+          ]),
     );
   }
 }
