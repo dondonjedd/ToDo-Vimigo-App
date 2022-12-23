8da404d713cada9414cfd9c50a35978177c39b2c
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-23 10:04:02 +0800

    add date and description button to bottom sheet

--- lib/Views/Widgets/addTask_bottomSheet.dart
+++ lib/Views/Widgets/addTask_bottomSheet.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AddNewTask extends StatelessWidget {
-  AddNewTask({super.key});
+class AddNewTask extends StatefulWidget {
+  const AddNewTask({super.key});
 
+  @override
+  State<AddNewTask> createState() => _AddNewTaskState();
+}
+
+class _AddNewTaskState extends State<AddNewTask> {
   final _titleController = TextEditingController();
+  DateTime? _chosenDate;
 
   submitData() {
     final enteredTitle = _titleController.text;
@@ -11,25 +18,58 @@ class AddNewTask extends StatelessWidget {
     if (enteredTitle.isEmpty) {
       return;
     }
+  }
 
-    
+  _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(DateTime.now().year),
+            lastDate: DateTime.now())
+        .then((value) {
+      if (value == null) {
+        return;
+      }
+      setState(() {
+        _chosenDate = value;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding:
-            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          TextField(
-            decoration: const InputDecoration(labelText: "Title"),
-            autofocus: true,
-            controller: _titleController,
-            onSubmitted: (_) => submitData(),
-          ),
-        ]),
+    return Container(
+      margin: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: Card(
+        elevation: 3,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            TextField(
+              decoration: const InputDecoration(labelText: "Title"),
+              autofocus: true,
+              controller: _titleController,
+              onSubmitted: (_) => submitData(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _chosenDate == null
+                    ? const Text("No date chosen")
+                    : Text(DateFormat("dd/MM/yyyy").format(_chosenDate!)),
+                TextButton(
+                    onPressed: _presentDatePicker,
+                    child: Text(
+                      "Choose a date",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    )),
+                TextButton(
+                    onPressed: () {}, child: const Text("Add Description"))
+              ],
+            )
+          ]),
+        ),
       ),
     );
   }
