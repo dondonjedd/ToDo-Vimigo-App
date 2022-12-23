9b9209b52dd4d63f2bd3613e2f91d30b81a2cae0
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-23 09:33:45 +0800

    create floating add task button in Tasks screen

--- lib/Views/tabsScreen.dart
+++ lib/Views/tabsScreen.dart
@@ -30,26 +30,34 @@ class _TabsScreenState extends State<TabsScreen> {
     return ChangeNotifierProvider(
       create: (ctx) => Tasks(),
       child: Scaffold(
-        appBar:
-            AppBar(title: Text(_pages[_selectedPageIndex]["title"] as String)),
-        body: _pages[_selectedPageIndex]["page"] as Widget,
-        bottomNavigationBar: BottomNavigationBar(
-            onTap: _selectPage,
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            unselectedItemColor: Colors.cyan,
-            selectedItemColor: Colors.white,
-            currentIndex: _selectedPageIndex,
-            type: BottomNavigationBarType.fixed,
-            items: const [
-              BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.list,
+          appBar: AppBar(
+              title: Text(_pages[_selectedPageIndex]["title"] as String)),
+          body: _pages[_selectedPageIndex]["page"] as Widget,
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: _selectPage,
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              unselectedItemColor: Colors.cyan,
+              selectedItemColor: Colors.white,
+              currentIndex: _selectedPageIndex,
+              type: BottomNavigationBarType.fixed,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.list,
+                    ),
+                    label: "ToDos"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.calendar_month), label: "Calendar")
+              ]),
+          floatingActionButton: _selectedPageIndex == 0
+              ? FloatingActionButton(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  onPressed: (() {}),
+                  child: const Icon(
+                    Icons.add,
                   ),
-                  label: "ToDos"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.calendar_month), label: "Calendar")
-            ]),
-      ),
+                )
+              : null),
     );
   }
 }
