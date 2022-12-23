9ebe37d4acc3e74cde49807be4840cd064480bf7
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-23 14:26:43 +0800

    create dismissable tasks for completed items

--- lib/Views/Widgets/completed_tasklist.dart
+++ lib/Views/Widgets/completed_tasklist.dart
@@ -14,13 +14,34 @@ class CompletedTaskList extends StatelessWidget {
       shrinkWrap: true,
       physics: const ScrollPhysics(),
       itemBuilder: (ctx, index) {
-        return ListTile(
+        return Dismissible(
           key: ValueKey(completedTasks[index].id),
-          title: Text(completedTasks[index].title),
-          leading: checkBox(
-              TasksController()
-                  .getIndexWithId(context, completedTasks[index].id),
-              true),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            TasksController().removeTask(
+                context,
+                TasksController()
+                    .getIndexWithId(context, completedTasks[index].id));
+          },
+          background: Container(
+            color: Theme.of(context).colorScheme.error,
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.only(right: 20),
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            child: Icon(
+              Icons.delete,
+              color: Theme.of(context).colorScheme.inversePrimary,
+              size: 40,
+            ),
+          ),
+          child: ListTile(
+            key: ValueKey(completedTasks[index].id),
+            title: Text(completedTasks[index].title),
+            leading: checkBox(
+                TasksController()
+                    .getIndexWithId(context, completedTasks[index].id),
+                true),
+          ),
         );
       },
       itemCount: completedTasks.length,
