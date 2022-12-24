8e1beb537ac88f5ea8ec455edda73d35edde2f4f
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-24 16:41:27 +0800

    Change Date picker UI  and remove edit button - On back press will automatically edit task - in edit task screen

--- lib/Views/editTask_screen.dart
+++ lib/Views/editTask_screen.dart
@@ -17,6 +17,13 @@ class _EditTaskState extends State<EditTask> {
   final _form = GlobalKey<FormState>();
   var _taskToEdit = Task(id: "", title: "");
   var _isInit = false;
+  final _dateController = TextEditingController();
+  final _datePickerNode = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   void didChangeDependencies() {
@@ -24,6 +31,11 @@ class _EditTaskState extends State<EditTask> {
       final taskIndex = ModalRoute.of(context)?.settings.arguments as int;
       _taskToEdit = TasksController().getTaskAtIndex(context, taskIndex);
       _chosenDate = _taskToEdit.date;
+      if (_chosenDate == null) {
+        _dateController.text = "No Date Chosen";
+      } else {
+        _dateController.text = DateFormat("dd/MM/yyyy").format(_chosenDate!);
+      }
     }
     _isInit = true;
 
@@ -31,14 +43,13 @@ class _EditTaskState extends State<EditTask> {
   }
 
   void _saveForm() {
-    if (!(_form.currentState?.validate())!) {
-      return;
-    }
+    // if (!(_form.currentState?.validate())!) {
+    //   return;
+    // }
     _form.currentState?.save();
 
     _taskToEdit = _taskToEdit.copyWith(id: _taskToEdit.id, date: _chosenDate);
     TasksController().updateTask(context, _taskToEdit.id, _taskToEdit);
-    Navigator.of(context).pop();
   }
 
   _presentDatePicker() {
@@ -53,6 +64,7 @@ class _EditTaskState extends State<EditTask> {
       }
       setState(() {
         _chosenDate = value;
+        _dateController.text = DateFormat("dd/MM/yyyy").format(_chosenDate!);
       });
     });
   }
@@ -60,82 +72,84 @@ class _EditTaskState extends State<EditTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: ElevatedButton(
-        style: ButtonStyle(
-            backgroundColor: MaterialStatePropertyAll(
-                Theme.of(context).colorScheme.primary)),
-        onPressed: () {
-          _saveForm();
-        },
-        child: const Icon(Icons.edit),
-      ),
+      // floatingActionButton: ElevatedButton(
+      //   style: ButtonStyle(
+      //       backgroundColor: MaterialStatePropertyAll(
+      //           Theme.of(context).colorScheme.primary)),
+      //   onPressed: () {
+      //     _saveForm();
+      //   },
+      //   child: const Icon(Icons.edit),
+      // ),
       appBar: AppBar(title: const Text("View/Edit Task")),
-      body: Container(
-        margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-        child: Form(
-            key: _form,
-            child: ListView(children: [
-              TextFormField(
-                maxLength: 60,
-                maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                textAlign: TextAlign.center,
-                initialValue: _taskToEdit.title,
-                decoration: const InputDecoration(
-                    labelText: "Title",
-                    floatingLabelAlignment: FloatingLabelAlignment.center),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return "Please enter a valid title";
-                  }
-                  return null;
-                },
-                onFieldSubmitted: (_) => _saveForm(),
-                onSaved: (newValue) =>
-                    {_taskToEdit = _taskToEdit.copyWith(title: newValue)},
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _chosenDate == null
-                      ? const Text(
-                          "No Date Chosen",
-                        )
-                      : Text(
-                          "Chosen Date: ${DateFormat("dd/MM/yyyy").format(_chosenDate!)}"),
-                  ElevatedButton(
-                      onPressed: _presentDatePicker,
-                      child: const Text(
-                        "Choose a date",
-                      )),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                maxLength: 250,
-                maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                initialValue: _taskToEdit.description,
-                decoration: const InputDecoration(
-                  labelText: "Description",
-                  border: InputBorder.none,
-                  floatingLabelAlignment: FloatingLabelAlignment.center,
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.blue,
+      body: WillPopScope(
+        onWillPop: () async {
+          _saveForm();
+          return true;
+        },
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+          child: Form(
+              key: _form,
+              child: ListView(children: [
+                TextFormField(
+                  maxLength: 60,
+                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  textAlign: TextAlign.center,
+                  initialValue: _taskToEdit.title,
+                  decoration: const InputDecoration(
+                      labelText: "Title",
+                      floatingLabelAlignment: FloatingLabelAlignment.center),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter a valid title";
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (_) => _saveForm(),
+                  onSaved: (newValue) =>
+                      {_taskToEdit = _taskToEdit.copyWith(title: newValue)},
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                    focusNode: _datePickerNode,
+                    readOnly: true,
+                    controller: _dateController,
+                    onTap: _presentDatePicker,
+                    decoration: const InputDecoration(
+                      labelText: "Date",
+                      border: InputBorder.none,
+                      // suffixIconColor: Theme.of(context).colorScheme.primary,
+                      prefixIcon: Icon(Icons.edit),
+                    )),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  maxLength: 250,
+                  maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  initialValue: _taskToEdit.description,
+                  decoration: const InputDecoration(
+                    labelText: "Description",
+                    border: InputBorder.none,
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                      ),
                     ),
                   ),
+                  minLines: 1,
+                  maxLines: 15,
+                  onFieldSubmitted: (_) => _saveForm(),
+                  onSaved: (newValue) => {
+                    _taskToEdit = _taskToEdit.copyWith(description: newValue)
+                  },
                 ),
-                minLines: 1,
-                maxLines: 15,
-                onFieldSubmitted: (_) => _saveForm(),
-                onSaved: (newValue) =>
-                    {_taskToEdit = _taskToEdit.copyWith(description: newValue)},
-              ),
-            ])),
+              ])),
+        ),
       ),
     );
   }
